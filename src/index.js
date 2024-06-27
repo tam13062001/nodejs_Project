0f6a31f0a1c7c6bb3151a28a30b6eab77cd22f70
tam13062001 <dtam768@gmail.com>
2024-06-27 14:35:50 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded());
+
 // HTTP Logger
 app.use(morgan('combined'));
 
@@ -24,7 +26,19 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+  console.log(req.query.q);
   res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+
+  console.log(req.body);
+
+  res.send('');
 })
 
 app.listen(port, () => {
